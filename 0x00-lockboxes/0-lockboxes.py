eac58e786866eb392c6492b5f39a23bc2bda4c92
Nic Basilio <bnbasilio@gmail.com>
2020-12-11 05:56:57 +0000

    task 0 completed

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''' 0-lockboxes.py '''
+
+def canUnlockAll(boxes):
+
+    if not boxes:
+        return False
+    open = [False] * len(boxes)
+    open[0] = True
+    keys = [0]
+    while keys:
+        checked = keys.pop()
+        for key in boxes[checked]:
+            if key < len(boxes) and not open[key]:
+                open[key] = True
+                keys.append(key)
+    result = all(open)
+    return result
